3fd708a5296d6541f0eee30fd094327ab3ec0e7a
weraclitof <weraclitof@gmail.com>
2021-06-09 10:42:02 -0300

    feat(ourservices-flow): Create model for OurServices

---
+++ server/models/Ourservices.js
@@ -0,0 +1,24 @@
+const keystone = require('keystone');
+
+const { Types } = keystone.Field;
+
+const ourservices = new keystone.List('ourservices');
+
+Ourservices.add({
+  name: {
+    type: Types.Text,
+    required: true,
+    initial: true,
+    index: true,
+  },
+  image: {
+    type: Types.CloudinaryImages,
+    require: true,
+  },
+  discription: {
+      type: types.text,
+      require: true,
+  },
+});
+
+Ourservices.register();
